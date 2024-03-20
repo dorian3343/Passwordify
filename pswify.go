175a2018d352bbf7477b91ff586f16a852cb4bf4
dorian3343 <dorian3343@gmail.com>
2024-03-20 21:08:23 +0100

    added special character generation

--- pswify.go
+++ pswify.go
@@ -10,13 +10,13 @@ import (
 )
 
 type State struct {
-	CharCount int
-	NumCount  int
-	Password  string
+	CharCount   int
+	NumCount    int
+	SymbolCount int
 }
 
 func NewState() State {
-	return State{CharCount: 12, NumCount: 0, Password: ""}
+	return State{CharCount: 12, NumCount: 0, SymbolCount: 0}
 }
 func randomI() int {
 	min := 0
@@ -30,12 +30,21 @@ func randomS() string {
 	str := charset[random.Intn(len(charset))]
 	return string(str)
 }
+func randomC() string {
+	const charset = "!@#$%^&*()_+{}|?><.,/;"
+	seed := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(seed)
+	str := charset[random.Intn(len(charset))]
+	return string(str)
+}
+
 func main() {
 	var str strings.Builder
 	state := NewState()
 
 	// CLI flags
 	numCountPtr := flag.Int("num", 0, "number of numbers in the generated password")
+	symbolCountPtr := flag.Int("symboli", 12, "number of special characters in the generated password")
 	charCountPtr := flag.Int("char", 12, "number of characters in the generated password")
 	flag.Parse()
 
@@ -53,12 +62,16 @@ func main() {
 	if flagCount >= 1 && flags[0] == "gen" {
 		state.NumCount = *numCountPtr
 		state.CharCount = *charCountPtr
+		state.SymbolCount = *symbolCountPtr
 		for i := 0; i < state.CharCount; i++ {
 			str.WriteString(randomS())
 		}
 		for i := 0; i < state.NumCount; i++ {
 			num := randomI()
 			str.WriteString(strconv.Itoa(num))
+		}
+		for i := 0; i < state.SymbolCount; i++ {
+			str.WriteString(randomC())
 		}
 		fmt.Println("Your generated password is:\n------------------------\n" + str.String())
 	}
